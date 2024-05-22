44c52a7e73b866e0530e105b71047a72f498ddff
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2024-05-22 14:03:46 +0300

    Update initia.sh

--- initia.sh
+++ initia.sh
@@ -103,19 +103,15 @@ sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $
 sed -i "s/snapshot-interval *=.*/snapshot-interval = 0/g" $HOME/.initia/config/app.toml
 
 #update
-sed -i \
-  -e 's/timeout_commit = ".*"/timeout_commit = "1s"/g' \
-  -e 's/timeout_propose = ".*"/timeout_propose = "3s"/g' \
-  -e 's/timeout_propose_delta = ".*"/timeout_propose_delta = "500ms"/g' \
-  -e 's/timeout_prevote = ".*"/timeout_prevote = "1s"/g' \
-  -e 's/timeout_prevote_delta = ".*"/timeout_prevote_delta = "500ms"/g' \
-  -e 's/timeout_precommit = ".*"/timeout_precommit = "1s"/g' \
-  -e 's/timeout_precommit_delta = ".*"/timeout_precommit_delta = "500ms"/g' \
-  $HOME/.initia/config/config.toml
+sed -i -e "s/^timeout_commit *=.*/timeout_commit = \"1s\"/" $HOME/.initia/config/config.toml
+sed -i -e "s/^timeout_propose *=.*/timeout_propose = \"3s\"/" $HOME/.initia/config/config.toml
+sed -i -e "s/^timeout_propose_delta *=.*/timeout_propose_delta = \"500ms\"/" $HOME/.initia/config/config.toml
+sed -i -e "s/^timeout_prevote *=.*/timeout_prevote = \"1s\"/" $HOME/.initia/config/config.toml
+sed -i -e "s/^timeout_prevote_delta *=.*/timeout_prevote_delta = \"500ms\"/" $HOME/.initia/config/config.toml
+sed -i -e "s/^timeout_precommit *=.*/timeout_precommit = \"1s\"/" $HOME/.initia/config/config.toml
+sed -i -e "s/^timeout_precommit_delta *=.*/timeout_precommit_delta = \"500ms\"/" $HOME/.initia/config/config.toml
 
-sed -i \
-  -e 's/client_timeout = ".*"/client_timeout = "1s"/g' \
-  $HOME/.initia/config/app.toml
+sed -i -e "s|^client_timeout *=.*|client_timeout = \"1s\"|" $HOME/.initia/config/app.toml
 
 # enable prometheus
 sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.initia/config/config.toml
